f344fdad1eaf63f8fce5f7ca22f9dd52e0ce426f
SiteHUB-NG <infra-team@sitehub.agency>
2025-06-27 15:41:31 +0100

    Update servers.php alignment

--- config/servers.php
+++ config/servers.php
@@ -7,22 +7,22 @@
 return [
   [
     'id'          => 'Nigeria',
-	'name'        => 'Nigeria',
-	'description' => 'Our DC',
+    'name'        => 'Nigeria',	
+    'description' => 'Our DC',
     'country'     => 'ng',
     'endpoint'    => 'https://lg_ng.our-company.ng/looking-glass-node/api.php'
   ],
   [
     'id'          => 'France',
-	'name'        => 'France',
-	'description' => 'Our DC',
+    'name'        => 'France',
+    'description' => 'Our DC',
     'country'     => 'fr',
     'endpoint'    => 'https://lg_fr.our-company.fr/looking-glass-node/api.php'
   ],
   [
     'id'          => 'United States',
-	'name'        => 'United States',
-	'description' => 'Our DC',
+    'name'        => 'United States',
+    'description' => 'Our DC',
     'country'     => 'us',
     'endpoint'    => 'https://lg_us.our-company.us/looking-glass-node/api.php'
   ]
